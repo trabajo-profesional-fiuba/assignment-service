9141f4c5e083c8c5bc911c1852d0321098922f2d
ivanpfaab <pfaabivan@gmail.com>
2024-05-17 19:07:14 -0300

    test: happy edge case, 3 topic, 3 groups, 3 professors

--- test/simplex/test_logical.py
+++ test/simplex/test_logical.py
@@ -1,5 +1,6 @@
 """Module testing logical of simplex algorithm."""
 from src.algorithms.simplex_solver import SimplexSolver
+#from src.entities.group import Group
 from test.simplex.helper import create_vector, get_all_entities, get_teams_topics, get_topics, get_topics_tutors
 
 def test_more_teams_than_tutors_without_enough_capacity_so_there_are_teams_without_tutor():
@@ -35,8 +36,27 @@ def test_more_teams_than_tutors_but_with_enough_capacity_so_all_teams_are_assign
 
     solver = SimplexSolver(None, team_topic, topic_tutor, None)
     teams_topics_tutors = solver.solve_simplex(team_topic, topic_tutor, topics_c)  # Run the optimization
-    
-    print("PRINTING RESULT", teams_topics_tutors)
 
     #Improve assert to be more accurate and avoid edge cases
     assert len(teams_topics_tutors.items()) == 3
+
+def test_equal_teams_and_tutors_but_tutors_do_not_exceed_their_capacities():
+    """Testing that tutors get all teams without exceeding their capacities."""
+    num_groups = 3
+    num_topics = 6
+    num_tutors = 3
+    tutor_capacities = create_vector(num_tutors, 1)
+    topic_capacities = create_vector(num_topics, 1)
+    teams, topics, tutors = get_all_entities(num_groups, num_topics, num_tutors)
+
+    team_topic = get_teams_topics(teams, topics)
+    topic_tutor = get_topics_tutors(topics, tutors, tutor_capacities) # pylint: disable=line-too-long
+    topics_c = get_topics(topics, topic_capacities)
+
+    solver = SimplexSolver(None, team_topic, topic_tutor, None)
+    teams_topics_tutors = solver.solve_simplex(team_topic, topic_tutor, topics_c)  # Run the optimization
+
+    #Improve assert to be more accurate and avoid edge cases
+    assert len(teams_topics_tutors.items()) == 3
+
+
