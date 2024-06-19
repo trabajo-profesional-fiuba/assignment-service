5d891ea33d99db085e7f476a9f8d0de32791199b
celedituro <cdituro@fi.uba.ar>
2024-06-18 23:46:57 -0300

    doc: add api documentation

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 
 @app.route("/")
 def hello_world():
+    """
+    A simple endpoint that returns "Hello, World!"
+    ---
+    responses:
+      200:
+        description: A successful response
+        content:
+          text/plain:
+            schema:
+              type: string
+    """
     return "Hello, World!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
