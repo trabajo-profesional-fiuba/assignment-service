363e50a51ea1d4ae87bde6cfe2ea397d333f4ebf
celedituro <cdituro@fi.uba.ar>
2024-06-23 18:08:41 -0300

    refactor: service

--- api/service.py
+++ api/service.py
@@ -1,48 +1,61 @@
 from api.models import TopicPreferencesItem, TopicPreferencesUpdatedItem
 from api.repository import Repository
 from api.exceptions import TopicPreferencesDuplicated
+from typing import Union
 
 
 class Service:
     def __init__(self, repository: Repository):
         self._repository = repository
 
+    def create_items(self, emails: list, item: TopicPreferencesItem):
+        created_items = []
+        for email in emails:
+            if email:
+                created_items.append(
+                    self._repository.add_topic_preferences(email, item)
+                )
+        return created_items
+
     def add_topic_preferences(self, topic_preferences: TopicPreferencesItem):
         try:
-            item_1 = self._repository.add_topic_preferences(
-                topic_preferences.email, topic_preferences
+            new_items = self.create_items(
+                [
+                    topic_preferences.email,
+                    topic_preferences.email_student_group_2,
+                    topic_preferences.email_student_group_3,
+                    topic_preferences.email_student_group_4,
+                ],
+                topic_preferences,
             )
-            item_2 = self._repository.add_topic_preferences(
-                topic_preferences.email_student_group_2, topic_preferences
-            )
-            item_3 = self._repository.add_topic_preferences(
-                topic_preferences.email_student_group_3, topic_preferences
-            )
-            item_4 = self._repository.add_topic_preferences(
-                topic_preferences.email_student_group_4, topic_preferences
-            )
-            return [item_1, item_2, item_3, item_4]
+            return new_items
         except TopicPreferencesDuplicated as err:
             raise err
         except Exception as err:
             raise err
 
+    def update_items(self, emails: list, item: TopicPreferencesUpdatedItem):
+        updated_items = []
+        for email in emails:
+            if email:
+                updated_items.append(
+                    self._repository.update_topic_preferences(email, item)
+                )
+        return updated_items
+
     def update_topic_preferences(
         self, email: str, topic_preferences_update: TopicPreferencesUpdatedItem
     ):
         try:
-            item_1 = self._repository.update_topic_preferences(
-                email, topic_preferences_update
+            updated_items = self.update_items(
+                [
+                    email,
+                    topic_preferences_update.email_student_group_2,
+                    topic_preferences_update.email_student_group_3,
+                    topic_preferences_update.email_student_group_4,
+                ],
+                topic_preferences_update,
             )
-            item_2 = self._repository.update_topic_preferences(
-                topic_preferences_update.email_student_group_2, topic_preferences_update
-            )
-            item_3 = self._repository.update_topic_preferences(
-                topic_preferences_update.email_student_group_3, topic_preferences_update
-            )
-            item_4 = self._repository.update_topic_preferences(
-                topic_preferences_update.email_student_group_4, topic_preferences_update
-            )
-            return [item_1, item_2, item_3, item_4]
+            return updated_items
         except Exception as err:
             raise err
