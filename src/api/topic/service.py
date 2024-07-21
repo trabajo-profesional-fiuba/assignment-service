7070f5cb14eeeda041e86a2ccd8f25ee1e93ae1a
celedituro <cdituro@fi.uba.ar>
2024-07-21 15:30:32 -0300

    refactor: clean code

--- src/api/topic/service.py
+++ src/api/topic/service.py
@@ -21,9 +21,3 @@ class TopicService:
             return self._repository.add_topic(topic)
         except Exception as err:
             raise err
-
-    def get_categories(self):
-        try:
-            return self._repository.get_categories()
-        except Exception as err:
-            raise err
