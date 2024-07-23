1c343cb4526788a51cbc06493e6c05347da213be
Alejo Villores <alejovillores@gmail.com>
2024-07-23 17:09:20 -0300

    add logging

--- src/config/config.py
+++ src/config/config.py
@@ -1,17 +1,46 @@
-import os
-from dotenv import load_dotenv
+from starlette.config import Config, environ
+from starlette.datastructures import Secret
 
 
-def get_configuration():
-    load_dotenv()
-    database_url = os.getenv("DATABASE_URL", None)
-    pool_size = os.getenv("DATABASE_POOL_SIZE", 10)
-    pool_timeout = os.getenv("DATABASE_TIMEOUT", 10)
+class ApiConfiguration:
+    # Config will be read from environment variables and/or ".env" files.
+    config = Config(".env")
 
-    config = {
-        "database_url": database_url,
-        "pool_size": pool_size,
-        "pool_timeout": pool_timeout,
-    }
+    """
+    The order in which configuration values are read is:
 
-    return config
+    - From an environment variable.
+    - From the .env file.
+    - The default value given in config.
+    - If none of those match, then config(...) will raise an error.
+    """
+
+    @property
+    def database_url(self) -> str:
+        return self.config("DATABASE_URL", default=None)
+
+    @property
+    def database_pool_size(self) -> int:
+        return self.config("DATABASE_POOL_SIZE", cast=int, default=10)
+
+    @property
+    def database_pool_timeout(self) -> int:
+        return self.config("DATABASE_TIMEOUT", cast=int, default=10)
+
+    @property
+    def logging_level(self) -> str:
+        return self.config("LOGGIN_LEVEL", default="INFO")
+
+    @property
+    def secret_key(self) -> Secret:
+        return self.config("SECRET", cast=Secret, default="INFO")
+
+    @property
+    def enviroment(self) -> str:
+        return self.config("ENVIRONMENT", cast=str, default="DEV")
+
+    def set_env(self, key: str, value):
+        environ[key.to_upper()] = value
+
+
+api_config = ApiConfiguration()
