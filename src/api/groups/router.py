e076039e8b23ca5ce73a30ccff8dee86bd5cec17
Alejo Villores <alejovillores@gmail.com>
2024-08-10 22:43:36 -0300

    add exception in routs

--- src/api/groups/router.py
+++ src/api/groups/router.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, UploadFile, Depends, status, Query
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 
+from src.api.groups.exceptions import GroupError
 from src.api.groups.repository import GroupRepository
 from src.api.groups.schemas import GroupRequest, GroupResponse
 from src.api.groups.service import GroupService
@@ -33,13 +34,25 @@ async def add_group(
     session: Annotated[Session, Depends(get_db)],
     period=Query(pattern="^[1|2]C20[0-9]{2}$", examples=["1C2024"]),
 ):
-    tutor_service = TutorService(TutorRepository(session))
-    topic_service = TopicService(TopicRepository(session))
-    group_service = GroupService(GroupRepository(session))
+    try:
+        tutor_service = TutorService(TutorRepository(session))
+        topic_service = TopicService(TopicRepository(session))
+        group_service = GroupService(GroupRepository(session))
 
-    tutor_period = tutor_service.get_tutor_period_from_email(period, group.tutor_email)
-    topic = topic_service.add_topic(group.topic.name, group.topic.category)
+        tutor_period = tutor_service.get_tutor_period_from_email(period, group.tutor_email)
+        topic = topic_service.add_topic(group.topic.name, group.topic.category)
 
-    return group_service.create_assigned_group(
-        group.students, tutor_period.id, topic.id
-    )
+        return group_service.create_assigned_group(
+            group.students, tutor_period.id, topic.id
+        )
+    except GroupError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=str(e)
+        )
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
+
