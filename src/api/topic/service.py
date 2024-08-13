7ea7ef78e4cc7e2e8d7bbe48b07e75390ab844a3
celedituro <cdituro@fi.uba.ar>
2024-08-13 16:44:00 -0300

    refactor & docs: change function name and add documentation

--- src/api/topic/service.py
+++ src/api/topic/service.py
@@ -11,12 +11,20 @@ class TopicService:
         self._topic_repository = topic_repository
 
     def _add_category(self, category_name: str, categories: list[Category]):
+        """
+        Add a category to the list if it hasn't been added yet.
+        Returns a list of unique categories.
+        """
         new_category = Category(name=category_name)
         if not any(category.name == category_name for category in categories):
             categories.append(new_category)
         return categories
 
     def _add_topic(self, topic_name: str, category_name: str, topics: list[Topic]):
+        """
+        Add a topic to the list if it hasn't been added yet.
+        Returns a list of unique topics.
+        """
         new_topic = Topic(name=topic_name, category=category_name)
         if not any(
             (topic.name == topic_name and topic.category == category_name)
@@ -38,9 +46,12 @@ class TopicService:
 
     def _get_info(self, rows):
         """
-        Processes a list of rows containing topic names and categories, and returns
-        two lists: one of unique categories and another of unique topics with their
-        respective categories.
+        Processes a list of rows containing topic names, categories, tutors y capacities.
+        Returns necessary information to create a topic:
+            - Unique categories list: to add categories.
+            - Unique topics list.
+            - Dictionary with tutor emails as keys and a a list of dictionaries with topic and
+            capacity.
         """
         categories = []
         topics = []
@@ -54,11 +65,16 @@ class TopicService:
             )
         return categories, topics, topics_by_tutor
 
-    def _get_content(self, csv: str):
+    def _get_csv_rows(self, csv: str):
+        """
+        Processes a CSV string and returns its content as rows of information.
+        """
         csv_file = TopicCsvFile(csv=csv)
         return csv_file.get_info_as_rows()
 
-    def _get_topics_and_capacities_by_tutor(self, tutor_email: str, topics_by_tutor: dict):
+    def _get_topics_and_capacities_by_tutor(
+        self, tutor_email: str, topics_by_tutor: dict
+    ):
         """
         Given a tutor's email, return a tuple containing two lists:
             - A list of topic names assigned to that tutor.
@@ -71,24 +87,36 @@ class TopicService:
             capacities.append(topic_info["capacity"])
         return topics, capacities
 
-    def _update_tutor_periods(
+    def _add_topic_tutor_periods(
         self, topics_by_tutor: dict, tutor_repository: TutorRepository
     ):
+        """
+        Add a topic tutor period entity for each topic of each tutor.
+        """
         for tutor, topics_list in topics_by_tutor.items():
-            tutor_topics, tutor_capacities = self._get_topics_and_capacities_by_tutor(tutor, topics_by_tutor)
+            tutor_topics, tutor_capacities = self._get_topics_and_capacities_by_tutor(
+                tutor, topics_by_tutor
+            )
             tutor_repository.add_topic_tutor_period(
                 tutor, tutor_topics, tutor_capacities
             )
 
     def _add_topics(self, topics, categories):
+        """
+        Add a list of capacities and a list of topics.
+        """
         self._topic_repository.add_categories(categories)
         return self._topic_repository.add_topics(topics)
 
     def create_topics_from_string(self, csv: str, tutor_repository: TutorRepository):
-        rows = self._get_content(csv)
+        """
+        Processes a CSV string to create topics, categories, and tutor-topic assignments.
+        Returns the list of topics added.
+        """
+        rows = self._get_csv_rows(csv)
         categories, topics, topics_by_tutor = self._get_info(rows)
         topics = self._add_topics(topics, categories)
-        self._update_tutor_periods(topics_by_tutor, tutor_repository)
+        self._add_topic_tutor_periods(topics_by_tutor, tutor_repository)
         return TopicList.model_validate(topics)
 
     def get_topics(self):
