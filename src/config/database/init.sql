f303ae75ce6be9618632ba9bfa8433acebdf4660
alejovillores <avillores@fi.uba.ar>
2024-08-22 22:13:51 -0300

    fix init.sql

--- src/config/database/init.sql
+++ src/config/database/init.sql
@@ -1,6 +1,11 @@
 INSERT INTO categories (name) VALUES ('default') ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO public.users(
-	name, last_name, email, password, role)
-	VALUES ('admin', 'admin', 'alejovillores@gmail.com', encode(sha256('admin123'), 'hex'), 'ADMIN')
-    ON CONFLICT (id) DO NOTHING;;
+INSERT INTO public.users (
+    name, last_name, email, password, role
+)
+SELECT 'admin', 'admin', 'alejovillores@gmail.com', encode(sha256('admin123'), 'hex'), 'ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM public.users
+    WHERE email = 'alejovillores@gmail.com'
+);
