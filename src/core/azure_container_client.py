f2dceb718410c2c1531f1d09638d9bdea6a9237c
alejovillores <avillores@fi.uba.ar>
2024-09-19 12:42:10 -0300

    add upload and download

--- src/core/azure_container_client.py
+++ src/core/azure_container_client.py
@@ -1,3 +1,4 @@
+from typing import IO, Iterable
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 
@@ -7,11 +8,29 @@ class AzureContainerClient:
         self._access_key = access_key
         self._container = container
 
-    def exists(self):
+    def _get_container_client(self):
         conn_str = self._access_key
         container_name = self._container
         container_client = ContainerClient.from_connection_string(
             conn_str=conn_str, container_name=container_name
         )
 
+        return container_client
+
+    def exists(self) -> bool:
+        container_client = self._get_container_client()
         return container_client.exists()
+
+    def upload(self, data: bytes | str | Iterable | IO, filename: str) -> BlobClient:
+        container_client = self._get_container_client()
+        blob = container_client.upload_blob(data=data, name=filename)
+
+        return blob
+
+    def download(self, blob_name: str, outputfilename: str):
+        container_client = self._get_container_client()
+        stream_downloader = container_client.download_blob(blob=blob_name)
+
+        with open(outputfilename, "xb") as file:
+            bytes = stream_downloader.readall()
+            file.write(bytes)
--- tests/unit/core/azure_container_client_test.py
+++ tests/unit/core/azure_container_client_test.py
@@ -3,6 +3,7 @@ import pytest
 from src.config.config import api_config
 from src.core.azure_container_client import AzureContainerClient
 
+
 class TestAzureContainerClient:
 
     @pytest.mark.unit
@@ -10,7 +11,55 @@ class TestAzureContainerClient:
         # Arrange
         container_name = api_config.container
         access_key = api_config.storage_access_key
-        az_client = AzureContainerClient(container=container_name, access_key=access_key)
+        az_client = AzureContainerClient(
+            container=container_name, access_key=access_key
+        )
 
-        # Act & Assert 
+        # Act & Assert
         assert az_client.exists() is True
+
+    @pytest.mark.integration
+    def test_upload_test_file_to_azure(self):
+
+        # Arrange
+        filename = "test_data.txt"
+        file_path = "tests/unit/core/test_data.txt"
+        container_name = api_config.container
+        access_key = api_config.storage_access_key
+        az_client = AzureContainerClient(
+            container=container_name, access_key=access_key
+        )
+
+        with open(file_path, "rb") as file:
+            content = file.read()
+
+        blob = az_client.upload(content, filename)
+
+        assert blob.blob_name == filename
+
+    @pytest.mark.integration
+    def test_download_test_file_to_azure(self):
+
+        # Arrange
+        filename = "test_data.txt"
+        outputfilename = "C:/dev/uba/assignment-service/tests/unit/core/download.txt"
+
+        expected_content = (
+            "This is a txt file just for uploading and downloading for azure storage"
+        )
+        container_name = api_config.container
+        access_key = api_config.storage_access_key
+        az_client = AzureContainerClient(
+            container=container_name, access_key=access_key
+        )
+
+        az_client.download(filename, outputfilename)
+
+        # Open the file and read its content
+        with open(outputfilename, "r") as file:
+            file_content = file.read().strip()  # Remove any leading/trailing whitespace
+
+        # Assert that the content matches the expected content
+        assert (
+            file_content == expected_content
+        ), f"Content does not match. Expected: '{expected_content}', Found: '{file_content}'"
