d8cca60e2865b02187d4962232794158315d1016
alejovillores <avillores@fi.uba.ar>
2024-09-28 15:24:26 -0300

    add cases of multiple users

--- src/core/email_client.py
+++ src/core/email_client.py
@@ -1,35 +1,52 @@
-
 import sendgrid
 from sendgrid.helpers.mail import Mail, Email, To, Content
 from src.config.logging import logger
+
 
 class SendGridEmailClient:
 
     def __init__(self, api_key: str = None):
         self.api_key = api_key
-        self.sender = 'fiuba.tpp.notificaciones@gmail.com'
+        self.sender = "fiuba.tpp.notificaciones@gmail.com"
+        self.name = 'FIUBA Trabajo Profesional'
 
     def _get_api_client(self):
         if self.api_key is None:
-            raise Exception('No api key provided')
-        return sendgrid.SendGridAPIClient(api_key = self.api_key)
+            raise Exception("No api key provided")
+        return sendgrid.SendGridAPIClient(api_key=self.api_key)
 
     def _log_response(self, response):
         if response.status_code == 202:
             logger.info("Email send successfully")
         else:
-            logger.info(f"Sendgrid send email had a problem, the response code status is: {response.status_code}")
-
-    def send_email(self, to: str, subject: str, body:str):
+            logger.info(
+                f"Sendgrid send email had a problem, the response code status is: {response.status_code}"
+            )
+    
+    def send_mail(self, mail: Mail):
         sg = self._get_api_client()
-        from_email = Email(self.sender)  
-        to_email = To(to) 
-        content = Content("text/plain", body)
-        mail = Mail(from_email, to_email, subject, content)
-
+        
         # Get a JSON-ready representation of the Mail object
         mail_json = mail.get()
         response = sg.client.mail.send.post(request_body=mail_json)
+        return response
+
+    def send_email(self, to: str, subject: str, body: str):
+        from_email = Email(self.sender, name= self.name)
+        to_email = To(to)
+        content = Content("text/plain", body)
+        mail = Mail(from_email, to_email, subject, content)
+
+        response = self.send_mail(mail)
         self._log_response(response)
         return response.status_code
 
+    def send_emails(self, to: list[str], subject: str, body: str):
+        from_email = Email(self.sender, name= self.name)
+        to_emails = [To(user) for user in to]
+        content = Content("text/plain", body)
+        mail = Mail(from_email, to_emails, subject, content)
+
+        response = self.send_mail(mail)
+        self._log_response(response)
+        return response.status_code
--- tests/integration/core/email_client_test.py
+++ tests/integration/core/email_client_test.py
@@ -3,17 +3,38 @@ import pytest
 from src.config.config import api_config
 from src.core.email_client import SendGridEmailClient
 
+
 class TestEmailClient:
 
-    @pytest.mark.integration
+    # skip test so we dont send spam emails
+    @pytest.mark.skip
     def test_send_email_to_user(self):
         api_key = api_config.email_key
         email_client = SendGridEmailClient(api_key)
-        response = email_client.send_email('alejovillores@gmail.com', 'Test FIUBA', 'Hola como andas?')
+        response = email_client.send_email(
+            "avillores@fi.uba.ar ",
+            "Mensaje importante para Agus",
+            "Hola como andas?",
+        )
         assert response == 202
-    
-    @pytest.mark.integration
+
+    @pytest.mark.skip
     def test_send_api_withou_key_raise_exception(self):
         email_client = SendGridEmailClient()
         with pytest.raises(Exception):
-            response = email_client.send_email('test@fiuba.com', 'Test FIUBA', 'body')
+            response = email_client.send_email("test@fiuba.com", "Test FIUBA", "body")
+
+    @pytest.mark.skip
+    def test_send_email_to_multiple_users(self):
+        api_key = api_config.email_key
+        email_client = SendGridEmailClient(api_key)
+        users = [
+            "vlopez@fi.uba.ar",
+            "ipfaab@fi.uba.ar",
+            "cdituro@fi.uba.ar",
+            "joagomez@fi.uba.ar",
+        ]
+        response = email_client.send_emails(
+            users, "Test FIUBA", "Esto es una prueba no responder"
+        )
+        assert response == 202
