e29ddb632034d72d8d5cb8693f56b4cad7166d7c
alejovillores <avillores@fi.uba.ar>
2024-10-09 09:40:27 -0300

    add tests

--- tests/integration/api/dates/repository_test.py
+++ tests/integration/api/dates/repository_test.py
@@ -7,6 +7,7 @@ from src.api.dates.repository import DateSlotRepository
 from src.config.database.database import create_tables, drop_tables, engine
 from tests.integration.api.helper import ApiHelper
 
+
 class TestDateRepository:
     SessionFactory = sessionmaker(bind=engine)
     Session = scoped_session(SessionFactory)
@@ -21,15 +22,47 @@ class TestDateRepository:
 
     @pytest.mark.integration
     def test_insert_slot_into_dates(self, tables):
+        # Arrange
         helper = ApiHelper()
         helper.create_period("2C2024")
-        
+
         slot = dt.datetime(2024, 10, 8, 9, 0)
-        period = '2C2024'
+        period = "2C2024"
 
         repository = DateSlotRepository(self.Session)
         date = DateSlot(period_id=period, slot=slot)
-
+        # Act
         date_saved = repository.add_date_slot(date)
+        # Assert
         assert date_saved.period_id == date.period_id
         assert date_saved.slot == date.slot
+
+    @pytest.mark.integration
+    def test_bulk_insert_slot_into_dates(self, tables):
+        # Arrange
+        helper = ApiHelper()
+        helper.create_period("2C2024")
+
+        period = "2C2024"
+        slots = [
+            {
+                "period_id": period,
+                "slot": dt.datetime(2024, 10, 7, 16, 0),
+            },
+            {
+                "period_id": period,
+                "slot": dt.datetime(2024, 10, 7, 22, 0),
+            },  
+            {
+                "period_id": period,
+                "slot": dt.datetime(2024, 10, 8, 14, 0),
+            },
+            {
+                "period_id": period,
+                "slot": dt.datetime(2024, 10, 8, 18, 0),
+            },
+        ]
+
+        repository = DateSlotRepository(self.Session)
+        dates_saved = repository.bulk_insert(slots)
+        assert len(dates_saved) == 4
