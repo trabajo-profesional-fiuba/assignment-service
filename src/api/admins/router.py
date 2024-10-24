d4f24494052c9854466bce041ad60b0361a03d0b
alejovillores <avillores@fi.uba.ar>
2024-10-24 15:04:12 -0300

    doc in admins

--- src/api/admins/router.py
+++ src/api/admins/router.py
@@ -1,6 +1,6 @@
-from typing_extensions import Annotated
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from typing_extensions import Annotated
 
 from src.api.admins.schemas import AdminRequest
 from src.api.admins.service import AdminService
@@ -8,25 +8,18 @@ from src.api.auth.hasher import get_hasher, ShaHasher
 from src.api.auth.jwt import InvalidJwt, JwtResolver, get_jwt_resolver
 from src.api.auth.schemas import oauth2_scheme
 from src.api.auth.service import AuthenticationService
-
 from src.api.exceptions import Duplicated, ServerError
-
-
 from src.api.users.exceptions import InvalidCredentials
 from src.api.users.repository import UserRepository
 from src.api.users.schemas import UserResponse
-
 from src.api.utils.response_builder import ResponseBuilder
 from src.config.database.database import get_db
 
-
 router = APIRouter(prefix="/admins", tags=["Admins"])
-
 
 @router.post(
     "",
     response_model=UserResponse,
-    description="Creates a new admin",
     summary="Add a new admin",
     responses={
         status.HTTP_400_BAD_REQUEST: {"description": "Admin schema is not correct"},
@@ -43,6 +36,7 @@ async def add_admin(
     token: Annotated[str, Depends(oauth2_scheme)],
     jwt_resolver: Annotated[JwtResolver, Depends(get_jwt_resolver)],
 ):
+    """Endpoint para crear un nuevo administrador siendo administrador"""
     try:
         auth_service = AuthenticationService(jwt_resolver)
         auth_service.assert_only_admin(token)
--- src/api/admins/service.py
+++ src/api/admins/service.py
@@ -3,13 +3,13 @@ from src.api.admins.schemas import AdminRequest
 from src.api.exceptions import Duplicated
 from src.api.users.models import Role, User
 
-
 class AdminService:
 
     def __init__(self, repository) -> None:
         self._repository = repository
 
     def add_admin(self, hasher, admin: AdminRequest):
+        """Agrega un nuevo usuario con rol de administrador usando el id como password"""
         try:
             return self._repository.add_user(
                 User(
@@ -21,7 +21,7 @@ class AdminService:
                     role=Role.ADMIN,
                 )
             )
-        except Duplicated:
-            raise Duplicated("Duplicated student")
+        except Duplicated as e:
+            raise e
         except Exception:
             raise AdminNotInserted("Could not insert an admin in the database")
