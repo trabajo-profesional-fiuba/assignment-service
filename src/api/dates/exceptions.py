d79f140b7c26050fc2adc801e280b4e37c5a2e3e
alejovillores <avillores@fi.uba.ar>
2024-10-24 15:04:36 -0300

    doc in dates

--- src/api/dates/exceptions.py
+++ src/api/dates/exceptions.py
@@ -2,7 +2,7 @@ from fastapi import status
 from fastapi.exceptions import HTTPException
 
 
-# External
+# Externos
 class InvalidDate(HTTPException):
     def __init__(self, message: str):
         super().__init__(detail=message, status_code=status.HTTP_400_BAD_REQUEST)
--- src/api/dates/maper.py
+++ src/api/dates/maper.py
@@ -1,9 +1,10 @@
-from src.core.date_slots import DateSlot
 from src.api.dates.models import DateSlot as DateSlotModel
+from src.core.date_slots import DateSlot
 
 
 class DateSlotsMapper:
 
     @staticmethod
     def map_model_to_date_slot(db_dates: list[DateSlotModel]):
+        """Mapea una lista de fechas desde la base de datos hacia clases nativas de python"""
         return [DateSlot(start_time=date.slot) for date in db_dates]
