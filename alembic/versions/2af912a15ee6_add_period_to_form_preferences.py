c34b4f57c4e5899538bc0bb273d026f9007cd1f5
Vicky Lopez <vlopez@fi.uba.ar>
2024-11-02 17:47:10 -0300

    Arreglamos migraciones en FormPreferences

--- alembic/versions/2af912a15ee6_add_period_to_form_preferences.py
+++ alembic/versions/2af912a15ee6_add_period_to_form_preferences.py
@@ -24,8 +24,8 @@ def upgrade() -> None:
     op.add_column(
         "form_preferences", sa.Column("period_id", sa.String(), nullable=True)
     )
-    op.create_foreign_key(None, "form_preferences", "periods", ["period_id"], ["id"])
-
+    op.create_foreign_key(None, "form_preferences", "periods", ["period_id"], ["id"],ondelete="CASCADE")
+    
     # Actualizo las respuestas existentes
     op.execute(
         "UPDATE form_preferences SET period_id = '2C2024' WHERE period_id IS NULL"
